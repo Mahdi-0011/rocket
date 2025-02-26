b913e73bf782ec45d09b8e77a7d94f0ecf31cbc2
Niklas Mårdby <niklas.mardby@gmail.com>
2025-02-26 14:11:15 +0100

    Add SWPeopleCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import LukeCard from "./components/LukeCard/LukeCard";
+import SWPeopleCard from "./components/SWPeopleCard/SWPeopleCard";
 
 function App() {
 	return (
@@ -7,6 +8,9 @@ function App() {
 			<h1>rocket</h1>
 			<p>a demo of useEffect</p>
 			<LukeCard />
+			<SWPeopleCard id={2} />
+			<SWPeopleCard id={3} />
+			<SWPeopleCard id={22} />
 		</>
 	);
 }
---
+++ src/components/SWPeopleCard/SWPeopleCard.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import "./SWPeopleCard.scss";
+
+type SWPeopleCardProps = {
+	id: number;
+};
+
+type SWPeople = {
+	name: string;
+	height: string;
+	mass: string;
+	hair_color: string;
+	skin_color: string;
+	eye_color: string;
+	birth_year: string;
+	gender: string;
+	homeworld: string;
+	films: string[];
+	species: string[];
+	vehicles: string[];
+	starships: string[];
+	created: string;
+	edited: string;
+	url: string;
+};
+
+export default function SWPeopleCard({ id }: Readonly<SWPeopleCardProps>) {
+	const [loading, setLoading] = useState(true);
+	const [person, setPerson] = useState<SWPeople | null>(null);
+
+	useEffect(() => {
+		const url = `https://swapi.dev/api/people/${id}`;
+		let ignore = false;
+
+		const fetchData = async () => {
+			const result = await fetch(url);
+			const data = (await result.json()) as SWPeople;
+			if (!ignore) {
+				setPerson(data);
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+
+		return () => {
+			ignore = true;
+		};
+	}, []);
+
+	return (
+		<div className="SWPeopleCard">
+			<h2>SWPeopleCard</h2>
+			{loading ? (
+				<p>loading...</p>
+			) : (
+				<>
+					<p>Name: {person?.name}</p>
+					<p>Height: {person?.height} cm</p>
+					<p>Mass: {person?.mass} kg</p>
+					<p>Eye color: {person?.eye_color}</p>
+					<p>Birth year: {person?.birth_year}</p>
+				</>
+			)}
+		</div>
+	);
+}
