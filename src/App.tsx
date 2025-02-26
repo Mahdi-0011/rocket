0d0b3c1e8335c4bc215e6f12ecf2f319f5d080e7
Niklas Mårdby <niklas.mardby@gmail.com>
2025-02-26 10:34:49 +0100

    Install first setup with Vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import LukeCard from "./components/LukeCard/LukeCard";
+
+function App() {
+	return (
+		<>
+			<h1>rocket</h1>
+			<p>a demo of useEffect</p>
+			<LukeCard />
+		</>
+	);
+}
+
+export default App;
