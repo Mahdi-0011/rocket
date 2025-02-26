2252214ff78de02af5df119ef68e6e539761e320
Niklas Mårdby <niklas.mardby@gmail.com>
2025-02-26 10:36:54 +0100

    Add LukeCard component

---
+++ src/components/LukeCard/LukeCard.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import "./LukeCard.scss";
+
+export default function LukeCard() {
+	const [loading, setLoading] = useState(true);
+	const [name, setName] = useState("");
+	const [height, setHeight] = useState("");
+	const [eyeColor, setEyeColor] = useState("");
+
+	useEffect(() => {
+		const url = "https://swapi.dev/api/people/2";
+		let ignore = false;
+
+		const fetchData = async () => {
+			const result = await fetch(url);
+			const data = await result.json();
+			if (!ignore) {
+				setName(data.name);
+				setHeight(data.height);
+				setEyeColor(data.eye_color);
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+
+		return () => {
+			ignore = true;
+		};
+	}, []);
+
+	return (
+		<div className="LukeCard">
+			<h2>Star Wars API - People</h2>
+			{loading ? (
+				<p>loading...</p>
+			) : (
+				<>
+					<p>Name: {name}</p>
+					<p>height: {height} cm</p>
+					<p>Eye color: {eyeColor}</p>
+				</>
+			)}
+		</div>
+	);
+}
