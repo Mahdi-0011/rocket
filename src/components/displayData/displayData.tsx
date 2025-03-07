7c14cca2dfd743d08aacbcb038ef180f3d967491
Mahdi <mahdi.mousavi0011@gmail.com>
2025-03-07 09:42:29 +0100

    displaydata added.

--- src/components/displayData/displayData.tsx
+++ src/components/displayData/displayData.tsx
@@ -1,15 +1,15 @@
 
 import useFetch from "../../usefetch/usefetch";
 
-const displayData = () =>{
+const DisPlayData = () =>{
   const {data, loading, error} = useFetch("https://swapi.dev/api/people/1")
   if (loading) return <p>loading...</p>
   if (error) return <p>Error: {error}</p>
 return(
   <div>
-    name:{data?.name}, height:{data?.height}
+    name:{data?.name}, height:{data?.height}, {data?.birth_year}
   </div>
 );
 };
 
-export default displayData;
+export default DisPlayData;
