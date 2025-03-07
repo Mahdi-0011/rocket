dd185e6b9684457ea7e6503f3291361a1aebed93
Mahdi <mahdi.mousavi0011@gmail.com>
2025-03-07 09:42:52 +0100

    app uppdated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.scss";
 import DisneyCharacterCard from "./components/DisneyCharacterCard/DisneyCharacterCard";
+import DisPlayData from "./components/displayData/displayData";
 import LukeCard from "./components/LukeCard/LukeCard";
 import SWPeopleCard from "./components/SWPeopleCard/SWPeopleCard";
+import DomRef from "./components/useref/useref";
 
 function App() {
 	return (
 		<>
 			<h1>rocket</h1>
+			<DisPlayData/>
+			<hr />
+			<DomRef />
+			{/*
 			<p>a demo of useEffect</p>
 			<div className="starwarscards">
 			<LukeCard id={1}/>
@@ -18,7 +24,7 @@ function App() {
 			<div className="disnycards">
 			<DisneyCharacterCard id={308} />
 			<DisneyCharacterCard id={20} />
-			</div>
+			</div>*/}
 		</>
 	);
 }
